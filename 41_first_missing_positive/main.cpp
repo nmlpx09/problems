a0e6de967f632b0043eee880388f45b21739b96b
nmlpx09 <nmlpx09@duck.com>
2026-04-21 22:26:12 +0300

    add 41

---
+++ 41_first_missing_positive/main.cpp
@@ -0,0 +1,68 @@
+/*
+
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+int f(const std::vector<int>& v) {
+    std::unordered_map<int, uint> m;
+    std::unordered_set<int> s;
+    int max = -1;
+    for (const auto& e : v) {
+        s.insert(e);
+        if (m.count(e) == 0) {
+            m[e] = 1;
+        } else {
+            ++m[e];
+        }
+        if (m.count(e + 1) == 0) {
+            m[e + 1] = 1;
+        } else {
+            ++m[e + 1];
+        }
+        max = e > max ? e : max;
+    }
+    int r = max + 1;
+    for (const auto& [e, c]: m) {
+        if (e <= 0) {
+            continue;
+        }
+        if (c == 1 && s.count(e) == 0) {
+           r = r < e ? r : e;
+        }
+    }
+    if (r > 1 && s.count(1) == 0) {
+        return 1;
+    }
+    return r;
+}
+
+int main() {
+    std::vector<int>  v = {7,8,9,11,12};
+    std::cout << f(v);
+    return 0;
+}
